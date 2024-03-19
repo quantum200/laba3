8419d6461c11ad99156fe9d1bd2269b7a4b56703
mogkolya <95872473+quantum200@users.noreply.github.com>
2024-03-19 20:55:53 +0200

    Add files via upload
    додаю лабу

---
+++ laba2/zad1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+class DecimalCounter
+{
+    private int minValue; // Мінімальне значення лічильника
+    private int maxValue; // Максимальне значення лічильника
+    private int currentValue; // Поточне значення лічильника
+    public DecimalCounter()
+    {
+        minValue = 0;
+        maxValue = 100;
+        currentValue = minValue; // Початкове значення - мінімальне
+    }
+    public DecimalCounter(int min, int max)
+    {
+        minValue = min;
+        maxValue = max;
+        currentValue = minValue; // Початкове значення - мінімальне
+    }
+    public void Increment() // Метод для збільшення значення лічильника
+    {
+        if (currentValue < maxValue)
+            currentValue++;
+    }
+    public void Decrement() // Метод для зменшення значення лічильника
+    {
+        if (currentValue > minValue)
+            currentValue--;
+    }
+    public int GetValue() // Метод-геттер для отримання поточного значення лічильника
+    {
+        return currentValue;
+    }
+    public void SaveToJson(string filePath) // Метод для збереження об'єкта у JSON файл
+    {
+        string jsonString = JsonSerializer.Serialize(this);
+        File.WriteAllText(filePath, jsonString);
+    }
+    public static DecimalCounter LoadFromJson(string filePath) // Метод для завантаження об'єкта з JSON файлу
+    {
+        string jsonString = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<DecimalCounter>(jsonString);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        DecimalCounter counter1 = new DecimalCounter();
+        counter1.SaveToJson("counter1.json"); // Зберігаємо об'єкт у JSON файл
+        DecimalCounter counterLoaded = DecimalCounter.LoadFromJson("counter1.json"); // Завантажуємо об'єкт з JSON файлу
+        Console.WriteLine("Значення лiчильника: " + counterLoaded.GetValue()); // Використовує завантажений об'єкт
+        counterLoaded.Increment(); // Збільшення значення на одиницю
+        Console.WriteLine("Пiсля збiльшення: " + counterLoaded.GetValue());
+        counterLoaded.Decrement(); // Зменшення значення на одиницю
+        Console.WriteLine("Пiсля зменшення: " + counterLoaded.GetValue());
+        DecimalCounter counter2 = new DecimalCounter(5, 10); // Створення об'єкта класу DecimalCounter з власними значеннями меж діапазону
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Значення лiчильника: " + counter2.GetValue());
+        counter2.Increment(); // Збільшення значення на одиницю
+        Console.WriteLine("Пiсля збiльшення: " + counter2.GetValue());
+        counter2.Decrement(); // Зменшення значення на одиницю
+        Console.WriteLine("Пiсля зменшення: " + counter2.GetValue());
+    }
+}
